cfe899292ac15f7f351106032d51b69158fb6c50
Armand Deshais <deshais.armand@gmail.com>
2019-07-19 10:09:39 +0200

    Test Form + Maj JSON (add MDB Scss)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'digi-co';
+export class AppComponent implements OnInit {
+    title = 'Digi and Co';
+    myForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.myForm = this.fb.group({
+            nom: ['', Validators.required],
+            email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
+            message: ['', [Validators.required, Validators.minLength(15)]]
+        });
+    }
+
+    onSubmit(form: FormGroup) {
+        console.log('Valide ?', form.valid);
+        console.log('Nom', form.value.name);
+        console.log('Email', form.value.email);
+        console.log('Message', form.value.message);
+    }
 }
