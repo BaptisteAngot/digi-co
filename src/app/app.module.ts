814b909c742c1a0b8fbd0790ca3c84a90c62fdc7
Armand Deshais <deshais.armand@gmail.com>
2019-07-30 17:00:49 +0200

    formu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {FooterComponent} from './footer/footer.component';
+import {
+  ButtonsModule,
+  CheckboxModule,
+  IconsModule,
+  InputsModule,
+  InputUtilitiesModule,
+  ModalModule,
+  WavesModule
+} from 'angular-bootstrap-md';
+import { FcontactComponent } from './fcontact/fcontact.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        FooterComponent
-    ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    FcontactComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    InputsModule,
+    ButtonsModule,
+    WavesModule,
+    ModalModule,
+    IconsModule,
+    InputUtilitiesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
