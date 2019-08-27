9918e7a3555dd573aff449c80e3f4743a24cf2c6
Druxys <drudrux@gmail.com>
2019-08-27 16:06:49 +0200

    debug page articleé

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {FcontactComponent} from './fcontact/fcontact.component';
 import {ArticleComponent} from './article/article.component';
 import {IcontactComponent} from './icontact/icontact.component';
 import {WipComponent} from './wip/wip.component';
+import {ArticledeuxComponent} from './articledeux/articledeux.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,7 @@ const routes: Routes = [
   { path: 'accueil', component: HomeComponent },
   { path: 'contact', component: IcontactComponent },
   { path: 'article', component: ArticleComponent },
-  { path: 'video', component: ArticleComponent },
+  { path: 'video', component: ArticledeuxComponent },
   { path: 'wip', component: WipComponent },
 ];
 
@@ -21,4 +22,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, IcontactComponent, ArticleComponent, WipComponent]
+export const routingComponents = [HomeComponent, IcontactComponent, ArticleComponent, WipComponent, ArticledeuxComponent]
