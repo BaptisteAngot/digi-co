d416b7b4350c0f038527a03a887feaa630e5ea8e
Armand Deshais <deshais.armand@gmail.com>
2019-08-27 16:35:54 +0200

    Modif article video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {FcontactComponent} from './fcontact/fcontact.component';
 import {ArticleComponent} from './article/article.component';
-import {IcontactComponent} from "./icontact/icontact.component";
+import {IcontactComponent} from './icontact/icontact.component';
+import {ArticledeuxComponent} from './articledeux/articledeux.component';
 
 
 const routes: Routes = [
-  { path: '',  redirectTo: 'accueil', pathMatch: 'full' },
-  { path: 'accueil', component: HomeComponent },
-  { path: 'contact', component: IcontactComponent },
-  { path: 'article', component: ArticleComponent },
+  {path: '', redirectTo: 'accueil', pathMatch: 'full'},
+  {path: 'accueil', component: HomeComponent},
+  {path: 'contact', component: IcontactComponent},
+  {path: 'article', component: ArticleComponent},
+  {path: 'articlevideo', component: ArticledeuxComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, IcontactComponent, ArticleComponent]
+export class AppRoutingModule {
+}
+
+export const routingComponents = [HomeComponent, IcontactComponent, ArticleComponent, ArticledeuxComponent];
