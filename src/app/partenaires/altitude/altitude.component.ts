c1a7cd80d8097dfcf91faf55e6d4cf0cbf543f6c
Armand Deshais <deshais.armand@gmail.com>
2019-09-19 11:36:36 +0200

    Ajout contenu partenaires

--- src/app/partenaires/altitude/altitude.component.ts
+++ src/app/partenaires/altitude/altitude.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {NavigationEnd, Router} from '@angular/router';
+import {NgwWowService} from 'ngx-wow';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-altitude',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltitudeComponent implements OnInit {
 
-  constructor() { }
+  private wowSubscription: Subscription;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      this.wowService.init();
+    });
+  }
 
   ngOnInit() {
+    this.wowSubscription = this.wowService.itemRevealed$.subscribe(
+      (item: HTMLElement) => {
+      });
   }
 
 }
--- src/app/partenaires/resume/resume.component.ts
+++ src/app/partenaires/resume/resume.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { NgwWowService } from 'ngx-wow';
+import { Subscription } from 'rxjs/Subscription';
+import { filter } from 'rxjs/operators';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  private wowSubscription: Subscription;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      this.wowService.init();
+    });
+  }
 
   ngOnInit() {
+    this.wowSubscription = this.wowService.itemRevealed$.subscribe(
+      (item: HTMLElement) => {
+      });
   }
 
 }
