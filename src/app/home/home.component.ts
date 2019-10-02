5a219a60d50d71048c8626fe1b0348476ebff3c1
Druxys <drudrux@gmail.com>
2019-10-02 14:55:35 +0200

    debug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { filter } from 'rxjs/operators';
 import { ApiService } from '../services/api.service';
 declare var jQuery: any;
 import { Article } from '../shared/articles';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -29,6 +30,7 @@ export class HomeComponent implements OnInit {
       });
     this.api.getArticlesLimit()
       .subscribe((res: any) => {
+        console.log(res.articles.created_at);
         this.data = res.articles;
         console.log(this.data);
         this.isLoadingResults = false;
